8ffe38876ddcc003bcd9e9678bf2ebd490db70af
philbucher <philipp.bucher@tum.de>
2019-12-30 23:46:53 +0100

    return to initial version

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -2,9 +2,6 @@ import unittest
 import sys, os
 
 if __name__ == '__main__':
-    run_in_salome = ("SALOMEPATH" in os.environ)
-    if run_in_salome: print("\n\n"); sys.stdout.flush()
-
     verbosity = 0
     if len(sys.argv) == 2: # verbosity lvl was passed
         verbosity = int(sys.argv[1])
@@ -12,5 +9,4 @@ if __name__ == '__main__':
     tests = loader.discover(os.path.dirname(__file__)) # automatically discover all tests in this directory
     testRunner = unittest.runner.TextTestRunner(verbosity=verbosity)
     result = testRunner.run(tests).wasSuccessful()
-    if run_in_salome: print("\n\n"); sys.stdout.flush()
     sys.exit(not result) # retuning inverse bcs for sys.exit 1 aka true means error
