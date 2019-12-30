5a93b3c73179ec4fa70305bf5349461ff245ef35
philbucher <philipp.bucher@tum.de>
2019-12-31 00:28:10 +0100

    fixing

--- .github/run_tests_in_salome.py
+++ .github/run_tests_in_salome.py
@@ -6,9 +6,9 @@ script = "tests/run_all_tests.py"
 process = subprocess.Popen([
     command,
     script,
-    ], stdout=subprocess.PIPE, cwd=os.path.dirname(os.path.abspath(script)))
+    ], stdout=subprocess.PIPE)
 
-process_stdout, process_stderr = self.process.communicate()
+process_stdout, process_stderr = process.communicate()
 
 if process_stdout:
     print(process_stdout.decode('ascii'), file=sys.stdout)
@@ -17,7 +17,7 @@ if process_stderr:
 
 exitCode = int(process.returncode != 0)
 
-# some versions of salome (e.g. 8.3 & 8.4 don't have correct return codes, hence checking for error in log)
+# some versions of salome (e.g. 8.3 & 8.4) don't have correct return codes, hence checking for error in log
 # check is only done if no error was detected before
 if exitCode == 0 and "ERROR:salomeContext:SystemExit" in process_stdout:
     print("Detected failure in log")
