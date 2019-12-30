539b199c2021e2b362d76cfc0465bd820c4d5c6e
philbucher <philipp.bucher@tum.de>
2019-12-30 23:26:47 +0100

    minor

--- tests/assert_run_in_salome.py
+++ tests/assert_run_in_salome.py
@@ -2,5 +2,6 @@
 import os, sys
 
 run_in_salome = ("SALOMEPATH" in os.environ)
+print("Running in Salome:", run_in_salome)
 exit_code = int(not run_in_salome) # exit code of 0 means successful, but int(True) is 1, hence have to invert
 sys.exit(exit_code)
