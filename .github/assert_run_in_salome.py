5af9fffd02edd1d6e7b709737ec5ee6b5c5077e0
philbucher <philipp.bucher@tum.de>
2020-01-01 23:49:29 +0100

    minor

--- .github/assert_run_in_salome.py
+++ .github/assert_run_in_salome.py
@@ -1,12 +1,15 @@
 # Check to make sure the detection for if a script is run inside salome or not is working
 import os, sys
 
-if len(sys.argv) == 2: # verbosity lvl was passed
+if len(sys.argv) == 2:
     salome_execution = bool(sys.argv[1])
 else:
     raise Exception("Input whether or not this scipt is executed in Salome has to be given!")
 
+# detect wheter or not the script is run inside of Salome
 run_in_salome = ("SALOMEPATH" in os.environ)
+
 print("This script is executed in Salome:", salome_execution)
-print("Detection for running in Salome:", run_in_salome)
-sys.exit(not(salome_execution == run_in_salome))
+print("Detection for running in Salome:",   run_in_salome)
+
+sys.exit(int(not(salome_execution == run_in_salome))) # int(True) is 1 which means failure, hence have to invert
