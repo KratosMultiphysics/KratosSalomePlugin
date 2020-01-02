1a622cf1da3d2e2754ecf94e2964462cb0427ff0
philbucher <philipp.bucher@tum.de>
2020-01-03 00:31:12 +0100

    plugin can be loaded

--- plugin/salome_plugins.py
+++ plugin/salome_plugins.py
@@ -11,9 +11,34 @@
 '''
 This is the file that is detected by salome in order to load the plugin
 Do not rename or move this file!
+Check "salome_pluginsmanager.py" for more information
 '''
 
 ### settings for development/debugging
 logger_level = 2 # default value: 0
 reinitialize_data_handler = True # default value: False
 reload_modules = True # default value: False
+
+import salome_pluginsmanager
+# from utilities import utils
+# from utilities import salome_utilities
+
+def ReloadModules():
+    pass
+
+def InitializeKratosPlugin(context):
+    print("in InitializeKratosPlugin")
+
+### Registering the Plugin in Salome ###
+fct_args = [
+    'Kratos Multiphysics',
+    'Starting the plugin for Kratos Multiphysics',
+    InitializeKratosPlugin]
+
+# if salome_utilities.GetVersion() >= (9,3):
+#     from plugin_utilities import GetPluginFilePath
+#     from qtsalome import QIcon
+#     icon_file = GetPluginFilePath("pictures","kratos_logo.png")
+#     fct_args.append(QIcon(icon_file))
+
+salome_pluginsmanager.AddFunction(*fct_args)
