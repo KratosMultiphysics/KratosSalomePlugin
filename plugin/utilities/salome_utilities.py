58d8392bfc151ca2f12e52908b78f16cb68a765e
philbucher <philipp.bucher@tum.de>
2020-01-03 21:54:16 +0100

    adding salome_utils

---
+++ plugin/utilities/salome_utilities.py
@@ -0,0 +1,25 @@
+#  _  __         _          ___       _               ___ _           _
+# | |/ /_ _ __ _| |_ ___ __/ __| __ _| |___ _ __  ___| _ \ |_  _ __ _(_)_ _
+# | ' <| '_/ _` |  _/ _ (_-<__ \/ _` | / _ \ '  \/ -_)  _/ | || / _` | | ' \
+# |_|\_\_| \__,_|\__\___/__/___/\__,_|_\___/_|_|_\___|_| |_|\_,_\__, |_|_||_|
+#                                                               |___/
+# License: BSD License ; see LICENSE
+#
+# Main authors: Philipp Bucher
+#
+
+# This file must NOT have dependencies on other files in the plugin!
+
+# This file contains utility functions for interacting with salome
+
+import salome
+import salome_version
+
+def GetVersionMajor():
+    return int(salome_version.getVersionMajor())
+
+def GetVersionMinor():
+    return int(salome_version.getVersionMinor())
+
+def GetVersion():
+    return (GetVersionMajor(), GetVersionMinor())
