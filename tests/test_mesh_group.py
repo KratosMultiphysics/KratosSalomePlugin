be5c8f637ef79717dab851895b536e0e3e996bc0
philbucher <philipp.bucher@tum.de>
2020-01-31 00:01:14 +0100

    WIP test MeshGroup

--- tests/test_mesh_group.py
+++ tests/test_mesh_group.py
@@ -58,64 +58,14 @@ class TestMeshGroupObservers(unittest.TestCase):
         pass
 
 
-class TestMeshGroupMeshRelatedMethods(testing_utilities.SalomeTestCase):
-
-    def setUp(self):
-        super(TestMeshGroupMeshRelatedMethods, self).setUp()
-
-        # create geometry
-        O = self.geompy.MakeVertex(0, 0, 0)
-        OX = self.geompy.MakeVectorDXDYDZ(1, 0, 0)
-        OY = self.geompy.MakeVectorDXDYDZ(0, 1, 0)
-        OZ = self.geompy.MakeVectorDXDYDZ(0, 0, 1)
-        Box_1 = self.geompy.MakeBoxDXDYDZ(200, 200, 200)
-        [Face_1] = self.geompy.SubShapes(Box_1, [13])
-
-        self.geompy.addToStudy( O, 'O' )
-        self.geompy.addToStudy( OX, 'OX' )
-        self.geompy.addToStudy( OY, 'OY' )
-        self.geompy.addToStudy( OZ, 'OZ' )
-        self.geompy.addToStudy( Box_1, 'Box_1' )
-        self.geompy.addToStudyInFather( Box_1, Face_1, 'Face_1' )
-        # print(salome.ObjectToID(Box_1))
-
-        # PrintObjectInfo("Box_1", Box_1)
-        # PrintObjectInfo("salome", salome)
-        # PrintObjectInfo("self.study", self.study)
-
-        # create mesh
-        from salome.smesh import smeshBuilder
-        self.Mesh_1 = self.smesh.Mesh(Box_1)
-        Regular_1D = self.Mesh_1.Segment()
-        Max_Size_1 = Regular_1D.MaxSize(2)
-        MEFISTO_2D = self.Mesh_1.Triangle(algo=smeshBuilder.MEFISTO)
-        NETGEN_3D = self.Mesh_1.Tetrahedron()
-        start_time = time.time()
-        self.assertTrue(self.Mesh_1.Compute())
-        print("TIME TO Compute Mesh:", time.time() - start_time)
-        Sub_mesh_1 = self.Mesh_1.GetSubMesh( Face_1, 'Sub-mesh_1' )
-
-        ## Set names of Mesh objects
-        self.smesh.SetName(Regular_1D.GetAlgorithm(), 'Regular_1D')
-        self.smesh.SetName(NETGEN_3D.GetAlgorithm(), 'NETGEN 3D')
-        self.smesh.SetName(MEFISTO_2D.GetAlgorithm(), 'MEFISTO_2D')
-        self.smesh.SetName(Max_Size_1, 'Max Size_1')
-        self.smesh.SetName(self.Mesh_1.GetMesh(), 'Mesh_1')
-        self.smesh.SetName(Sub_mesh_1, 'Sub-mesh_1')
-
-        # print(dir(salome))
-        # print(dir(self.Mesh_1))
-        # PrintObjectInfo("self.Mesh_1", self.Mesh_1)
-        # PrintObjectInfo("self.Mesh_1.GetMesh()", self.Mesh_1.GetMesh())
-        # PrintObjectInfo("self.Mesh_1.mesh", self.Mesh_1.mesh)
-        # PrintObjectInfo("self.smesh", self.smesh)
-
-        # print(salome.ObjectToID(Sub_mesh_1))
-        # print(salome.ObjectToID(self.smesh.Mesh(self.Mesh_1)))
-
+class TestMeshGroupMeshRelatedMethods(testing_utilities.SalomeTestCaseWithBox):
 
     def test_GetNodes(self):
-        mesh_group = MeshGroup(self.__GetMainMeshID())
+        mesh_group = MeshGroup(salome.ObjectToID(self.sub_mesh_tetra_f_1))
+        PrintObjectInfo("self.study", self.study)
+        import salome_study
+        print(salome_study.DumpStudy())
+        print(self.mesh_tetra.GetID)
 
 
         start_time = time.time()
