26683536b19a1af512054183edaa45277ad40be7
philbucher <philipp.bucher@tum.de>
2020-01-31 23:25:13 +0100

    finished first version of GetNodesAndGeometricalEntities

--- plugin/mesh_group.py
+++ plugin/mesh_group.py
@@ -20,9 +20,9 @@ from utilities.utils import IsExecutedInSalome
 # note that this file is used a lot in the tests without salome, hence the import of salome-dependencies is done in a special way
 if IsExecutedInSalome():
     import salome
-    from salome.smesh import smeshBuilder
     import SMESH
     from utilities import salome_utilities
+    smesh = salome_utilities.GetSmesh()
 
 class MeshGroup(object):
     def __init__(self, mesh_identifier):
@@ -47,12 +47,12 @@ class MeshGroup(object):
 
     def GetNodes(self):
         if self.MeshExists():
+            start_time = time.time()
             current_mesh = salome_utilities.GetSalomeObject(self.mesh_identifier)
             if salome_utilities.IsSubMesh(current_mesh):
                 main_mesh = current_mesh.GetMesh()
             else:
                 main_mesh = current_mesh
-            start_time = time.time()
             nodes = {node_id : main_mesh.GetNodeXYZ(node_id) for node_id in current_mesh.GetNodesId()}
             logger.info('Getting {0} Nodes from Mesh "{1}" took {2:.2f} [s]'.format(len(nodes), self.GetMeshName(), time.time()-start_time))
             return nodes
@@ -71,38 +71,30 @@ class MeshGroup(object):
             if len(geometrical_entity_types) == 0:
                 return nodes, {}
 
-            geom_entities = {}
-
             start_time = time.time()
+
+            geom_entities = {}
+            mesh = salome_utilities.GetSalomeObject(self.mesh_identifier)
 
             entity_types_in_mesh = self.GetEntityTypesInMesh()
             for entity_type in geometrical_entity_types:
                 if entity_type in entity_types_in_mesh:
                     entities_filter = smesh.GetFilter(SMESH.ALL, SMESH.FT_EntityType,'=', entity_type)
-                    entities_ids = smesh.Mesh(mesh).GetIdsFromFilter(the_filter)
-
-                    geom_entities[geometrical_entity_types] = {ent_id : Mesh.GetElemNodes(ent_id) for ent_id in entities_ids} # TODO get the ids
+                    entities_ids = smesh.Mesh(mesh).GetIdsFromFilter(entities_filter)
+                    geom_entities[entity_type] = {ent_id : smesh.Mesh(mesh).GetElemNodes(ent_id) for ent_id in entities_ids}
                 else:
                     logger.warning('Entity type "{}" not in Mesh "{}"!'.format(str(entity_type)[7:], self.GetMeshName()))
                     geometrical_entity_types.remove(entity_type)
-                    geom_entities[geometrical_entity_types] = {}
+                    geom_entities[entity_type] = {}
 
-            logger.info('Getting {0} Geometrical Entities from Mesh "{1}" took {2:.2f} [s]'.format(len(nodes), self.GetMeshName(), time.time()-start_time))
-            return nodes, {}
+            logger.info('Getting {0} Geometrical Entities from Mesh "{1}" took {2:.2f} [s]'.format(sum([len(ge) for ge in geom_entities.values()]), self.GetMeshName(), time.time()-start_time))
+            return nodes, geom_entities
         else:
             return {}, {}
 
     def GetEntityTypesInMesh(self):
         if self.MeshExists():
             mesh = salome_utilities.GetSalomeObject(self.mesh_identifier)
-            fct_args = []
-            if salome_utilities.GetVersionMajor() < 9:
-                open_studies = salome.myStudyManager.GetOpenStudies()
-                num_open_studies = len(open_studies)
-                if num_open_studies != 1:
-                    logger.critical('More than one open study exists ({}), using the first one'.format(num_open_studies))
-                fct_args.append(salome.myStudyManager.GetStudyByName(open_studies[0]))
-            smesh = smeshBuilder.New(*fct_args)
             return [e for e, v in smesh.GetMeshInfo(mesh).items() if v > 0]
         else:
             return []
