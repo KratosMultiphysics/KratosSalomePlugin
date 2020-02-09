d3236bfec3ec8a7433982bd67ac6231dd88b3c10
philbucher <philipp.bucher@tum.de>
2020-02-09 12:51:07 +0100

    adding time info and color

--- tests/execute_in_salome.py
+++ tests/execute_in_salome.py
@@ -11,6 +11,7 @@
 # python imports
 import sys
 import subprocess
+import time, datetime
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
@@ -22,6 +23,8 @@ if __name__ == '__main__':
     info_msg += '    Arguments: {}\n'.format(sys.argv[3:])
     print(info_msg)
 
+    start_time = time.time()
+
     sp = subprocess.Popen("{} --shutdown-servers=1 -t {} args:{}".format(sys.argv[1], sys.argv[2], ", ".join([str(arg) for arg in sys.argv[3:]])), shell=True, stderr=subprocess.PIPE)
     _, process_stderr = sp.communicate()
 
@@ -30,4 +33,14 @@ if __name__ == '__main__':
 
     # salome < 8.5 does not return the correct return code, hence also check for error message
     ret_code = sp.returncode != 0 or "ERROR:salomeContext:SystemExit 1 in method _runAppli" in process_stderr.decode('ascii')
+
+    info_msg  = 'Execution took: {}'.format((str(datetime.timedelta(seconds=time.time()-start_time))).split(".")[0])
+    info_msg += ' and finished '
+    if ret_code:
+        info_msg += '\x1b[1;41mnot successful\x1b[0m'
+    else:
+        info_msg += '\x1b[1;42msuccessful\x1b[0m'
+
+    print(info_msg)
+
     sys.exit(ret_code)
