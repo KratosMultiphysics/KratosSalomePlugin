4fae38740a65640bd8952bb593a8353c3c899ea4
philbucher <philipp.bucher@tum.de>
2020-02-19 08:47:43 +0100

    using type hints

--- plugin/utilities/salome_utilities.py
+++ plugin/utilities/salome_utilities.py
@@ -32,10 +32,7 @@ def GetVersionMinor():
 def GetVersion():
     return (GetVersionMajor(), GetVersionMinor())
 
-def GetSalomeObjectReference(object_identifier, log_if_not_existing=True):
-    if not isinstance(object_identifier, str):
-        raise TypeError("Input is not a string!")
-
+def GetSalomeObjectReference(object_identifier: str, log_if_not_existing=True):
     obj_ref = salome.myStudy.FindObjectID(object_identifier)
 
     if obj_ref is None and log_if_not_existing:
@@ -43,37 +40,32 @@ def GetSalomeObjectReference(object_identifier, log_if_not_existing=True):
 
     return obj_ref
 
-def GetSalomeObject(object_identifier):
-    if not isinstance(object_identifier, str):
-        raise TypeError("Input is not a string!")
+def GetSalomeObject(object_identifier : str):
     return GetSalomeObjectReference(object_identifier).GetObject()
 
-def GetObjectName(object_identifier):
+def GetObjectName(object_identifier : str) -> str:
     return GetSalomeObjectReference(object_identifier).GetName()
 
-def ObjectExists(object_identifier):
+def ObjectExists(object_identifier : str) -> bool:
     return (GetSalomeObjectReference(object_identifier, False) is not None)
 
-def GetSalomeID(salome_object):
+def GetSalomeID(salome_object) -> str:
     return salome.ObjectToID(salome_object)
 
-def IsMesh(obj):
+def IsMesh(obj) -> bool:
     return isinstance(obj, salome.smesh.smeshBuilder.meshProxy)
 
-def IsSubMesh(obj):
+def IsSubMesh(obj) -> bool:
     return isinstance(obj, salome.smesh.smeshBuilder.submeshProxy)
 
-def IsMeshGroup(obj):
+def IsMeshGroup(obj) -> bool:
     # checking against "SMESH._objref_SMESH_GroupBase" includes the other three derived classes
     # - "SMESH._objref_SMESH_Group"
     # - "SMESH._objref_SMESH_GroupOnGeom"
     # - "SMESH._objref_SMESH_GroupOnFilter"
     return isinstance(obj, SMESH._objref_SMESH_GroupBase)
 
-def GetEntityType(name_entity_type):
-    if not isinstance(name_entity_type, str):
-        raise TypeError("Input is not a string!")
-
+def GetEntityType(name_entity_type : str):
     entity_types_dict = {str(entity_type)[7:] : entity_type for entity_type in SMESH.EntityType._items} # all entities avalable in salome
     return entity_types_dict[name_entity_type]
 
