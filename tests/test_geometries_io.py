569b364a973d340c7af9bfaedd84d488790f19c7
philbucher <philipp.bucher@tum.de>
2020-02-25 11:14:08 +0100

    adding more tests

--- tests/test_geometries_io.py
+++ tests/test_geometries_io.py
@@ -150,6 +150,7 @@ class TestGeometriesIOWithMockMeshInterfaces(object):
             geometries_io.GeometriesIO.AddMeshes(model_part, meshes)
 
             def CheckModelPart(model_part_to_check):
+                self.assertTrue(model_part_to_check.HasProperties(props_id))
                 self.assertEqual(len(the_nodes), model_part_to_check.NumberOfNodes())
                 for node in model_part_to_check.Nodes:
                     self.assertTrue(node.Id in the_nodes)
@@ -214,6 +215,7 @@ class TestGeometriesIOWithMockMeshInterfaces(object):
             geometries_io.GeometriesIO.AddMeshes(model_part, meshes)
 
             def CheckModelPart(model_part_to_check):
+                self.assertTrue(model_part_to_check.HasProperties(props_id))
                 self.assertEqual(len(nodes_mesh_1)+len(nodes_mesh_2), model_part_to_check.NumberOfNodes())
                 for node in model_part_to_check.Nodes:
                     if node.Id > 16:
@@ -244,6 +246,211 @@ class TestGeometriesIOWithMockMeshInterfaces(object):
                     self.assertListEqual(sorted(ref_geometries[elem.Id+id_offset]), nodes_id_list)
 
             self.__RecursiveCheckModelParts(model_part, model_part_name, CheckModelPart)
+
+        def test_add_to_existing_modelpart(self):
+            # in the other tests the (sub-)modelparts to which the entities are being don't exist yet
+            # in this test we make sure that it is possible to add entities to existing ModelParts
+            main_model_part = self._CreateModelPart()
+            sub_model_part = main_model_part.CreateSubModelPart("abc_sub")
+            the_nodes = {i+1 : [i+1,i*2,i+3.5] for i in range(15)}
+
+            attrs = { 'GetNodesAndGeometricalEntities.return_value': (the_nodes, {}) }
+            mesh_interface_mock = MagicMock(spec=MeshInterface)
+            mesh_interface_mock.configure_mock(**attrs)
+
+            meshes = [geometries_io.Mesh(sub_model_part.Name, mesh_interface_mock, {})]
+            geometries_io.GeometriesIO.AddMeshes(main_model_part, meshes)
+
+            def CheckModelPart(model_part_to_check):
+                self.assertEqual(len(the_nodes), model_part_to_check.NumberOfNodes())
+                for node in model_part_to_check.Nodes:
+                    self.assertTrue(node.Id in the_nodes)
+
+                    orig_node_coords = the_nodes[node.Id]
+                    self.assertAlmostEqual(orig_node_coords[0], node.X)
+                    self.assertAlmostEqual(orig_node_coords[1], node.Y)
+                    self.assertAlmostEqual(orig_node_coords[2], node.Z)
+
+            self.__RecursiveCheckModelParts(main_model_part, sub_model_part.Name, CheckModelPart)
+
+        def test_use_exsting_properties(self):
+            # in the other tests the properties that are used for the creation of element/conditions
+            # exist already. In this test the properties of the parent ModelPart are used
+            main_model_part = self._CreateModelPart()
+            sub_model_part = main_model_part.CreateSubModelPart("abc_sub")
+
+            geometry_name = "Line"
+            element_name = "Element2D2N"
+            props_id = 12
+
+            main_model_part.CreateNewProperties(props_id) # only exist in the main, but not yet in the submodelpart
+
+            mesh_description = { "elements" : {geometry_name : {element_name : props_id} } }
+
+            the_nodes = {i+1 : [i+1,i*2,i+3.5] for i in range(15)}
+            the_geom_entities = {geometry_name : {i+1 : [i+1, i+2] for i in range(14)}}
+
+            attrs = { 'GetNodesAndGeometricalEntities.return_value': (the_nodes, the_geom_entities) }
+            mesh_interface_mock = MagicMock(spec=MeshInterface)
+            mesh_interface_mock.configure_mock(**attrs)
+
+            meshes = [geometries_io.Mesh(sub_model_part.Name, mesh_interface_mock, mesh_description)]
+            geometries_io.GeometriesIO.AddMeshes(main_model_part, meshes)
+
+            def CheckModelPart(model_part_to_check):
+                self.assertTrue(model_part_to_check.HasProperties(props_id))
+                self.assertEqual(len(the_nodes), model_part_to_check.NumberOfNodes())
+                for node in model_part_to_check.Nodes:
+                    self.assertTrue(node.Id in the_nodes)
+
+                    orig_node_coords = the_nodes[node.Id]
+                    self.assertAlmostEqual(orig_node_coords[0], node.X)
+                    self.assertAlmostEqual(orig_node_coords[1], node.Y)
+                    self.assertAlmostEqual(orig_node_coords[2], node.Z)
+
+                self.assertEqual(len(the_geom_entities[geometry_name]), model_part_to_check.NumberOfElements())
+                for elem in model_part_to_check.Elements:
+                    # checking the connectivities
+                    self.assertTrue(elem.Id in the_geom_entities[geometry_name])
+                    nodes_id_list = sorted([node.Id for node in elem.GetNodes()])
+                    self.assertListEqual(sorted(the_geom_entities[geometry_name][elem.Id]), nodes_id_list)
+
+            self.__RecursiveCheckModelParts(main_model_part, sub_model_part.Name, CheckModelPart)
+
+        def test_add_entities_to_Root_and_SubModelPart(self):
+            # in this test we check if adding the same entities to a main and submodelpart works
+            # first we add the entities to the submodelpart, then to the parent modelpart
+            # the created entities must be the same!
+            main_model_part = self._CreateModelPart()
+            sub_model_part = main_model_part.CreateSubModelPart("abc_sub")
+
+            geometry_name = "Line"
+            element_name = "Element2D2N"
+            props_id = 3
+
+            mesh_description = { "elements" : {geometry_name : {element_name : props_id} } }
+
+            nodes = {i+1 : [i+1,i*2,i+3.5] for i in range(15)}
+            geometries = {geometry_name : {i+1 : [i+1, i+2] for i in range(14)}}
+            attrs = { 'GetNodesAndGeometricalEntities.return_value': (nodes, geometries) }
+
+            mesh_interface_mock_sub_mp = MagicMock(spec=MeshInterface)
+            mesh_interface_mock_sub_mp.configure_mock(**attrs)
+
+            mesh_interface_mock_main_mp = MagicMock(spec=MeshInterface)
+            mesh_interface_mock_main_mp.configure_mock(**attrs)
+
+            meshes = [
+                geometries_io.Mesh(sub_model_part.Name,  mesh_interface_mock_sub_mp,  mesh_description),
+                geometries_io.Mesh(main_model_part.Name, mesh_interface_mock_main_mp, mesh_description)
+            ]
+            geometries_io.GeometriesIO.AddMeshes(main_model_part, meshes)
+
+            def CheckModelPart(model_part_to_check):
+                self.assertTrue(model_part_to_check.HasProperties(props_id))
+                self.assertEqual(len(nodes), model_part_to_check.NumberOfNodes())
+                for node in model_part_to_check.Nodes:
+                    self.assertTrue(node.Id in nodes)
+
+                    orig_node_coords = nodes[node.Id]
+                    self.assertAlmostEqual(orig_node_coords[0], node.X)
+                    self.assertAlmostEqual(orig_node_coords[1], node.Y)
+                    self.assertAlmostEqual(orig_node_coords[2], node.Z)
+
+                self.assertEqual(len(geometries[geometry_name]), model_part_to_check.NumberOfElements())
+                for elem in model_part_to_check.Elements:
+                    # checking the connectivities
+                    ref_geometries = geometries[geometry_name]
+
+                    self.assertTrue(elem.Id in ref_geometries)
+                    nodes_id_list = sorted([node.Id for node in elem.GetNodes()])
+                    self.assertListEqual(sorted(ref_geometries[elem.Id]), nodes_id_list)
+
+            self.__RecursiveCheckModelParts(main_model_part, sub_model_part.Name, CheckModelPart)
+
+        def test_add_entities_to_different_SubModelPart(self):
+            # in this test we check if adding the same entities to two sibling-submodelpart works
+            # the entities are added one after the other, and the created entities must be the same!
+            main_model_part = self._CreateModelPart()
+            sub_model_part_1 = main_model_part.CreateSubModelPart("abc_sub")
+            sub_model_part_2 = main_model_part.CreateSubModelPart("def_sub")
+
+            geometry_name = "Line"
+            element_name = "Element2D2N"
+            props_id = 3
+
+            mesh_description = { "elements" : {geometry_name : {element_name : props_id} } }
+
+            nodes = {i+1 : [i+1,i*2,i+3.5] for i in range(15)}
+            geometries = {geometry_name : {i+1 : [i+1, i+2] for i in range(14)}}
+            attrs = { 'GetNodesAndGeometricalEntities.return_value': (nodes, geometries) }
+
+            mesh_interface_mock_sub_mp_1 = MagicMock(spec=MeshInterface)
+            mesh_interface_mock_sub_mp_1.configure_mock(**attrs)
+
+            mesh_interface_mock_sub_mp_2 = MagicMock(spec=MeshInterface)
+            mesh_interface_mock_sub_mp_2.configure_mock(**attrs)
+
+            meshes = [
+                geometries_io.Mesh(sub_model_part_1.Name, mesh_interface_mock_sub_mp_1, mesh_description),
+                geometries_io.Mesh(sub_model_part_2.Name, mesh_interface_mock_sub_mp_2, mesh_description)
+            ]
+            geometries_io.GeometriesIO.AddMeshes(main_model_part, meshes)
+
+            def CheckModelPart(model_part_to_check):
+                self.assertTrue(model_part_to_check.HasProperties(props_id))
+                self.assertEqual(len(nodes), model_part_to_check.NumberOfNodes())
+                for node in model_part_to_check.Nodes:
+                    self.assertTrue(node.Id in nodes)
+
+                    orig_node_coords = nodes[node.Id]
+                    self.assertAlmostEqual(orig_node_coords[0], node.X)
+                    self.assertAlmostEqual(orig_node_coords[1], node.Y)
+                    self.assertAlmostEqual(orig_node_coords[2], node.Z)
+
+                self.assertEqual(len(geometries[geometry_name]), model_part_to_check.NumberOfElements())
+                for elem in model_part_to_check.Elements:
+                    # checking the connectivities
+                    ref_geometries = geometries[geometry_name]
+
+                    self.assertTrue(elem.Id in ref_geometries)
+                    nodes_id_list = sorted([node.Id for node in elem.GetNodes()])
+                    self.assertListEqual(sorted(ref_geometries[elem.Id]), nodes_id_list)
+
+            self.__RecursiveCheckModelParts(main_model_part, sub_model_part_1.Name, CheckModelPart)
+            self.__RecursiveCheckModelParts(main_model_part, sub_model_part_2.Name, CheckModelPart)
+
+        def test_add_entities_different_properties_ids(self):
+            # checking if an error is thrown if the same entities are added with different properties Ids
+            main_model_part = self._CreateModelPart()
+            sub_model_part_1 = main_model_part.CreateSubModelPart("abc_sub")
+            sub_model_part_2 = main_model_part.CreateSubModelPart("def_sub")
+
+            geometry_name = "Line"
+            element_name = "Element2D2N"
+            props_id = 3
+
+            mesh_description = { "elements" : {geometry_name : {element_name : props_id} } }
+            mesh_description_2 = { "elements" : {geometry_name : {element_name : props_id+2} } }
+
+            nodes = {i+1 : [i+1,i*2,i+3.5] for i in range(15)}
+            geometries = {geometry_name : {i+1 : [i+1, i+2] for i in range(14)}}
+            attrs = { 'GetNodesAndGeometricalEntities.return_value': (nodes, geometries) }
+
+            mesh_interface_mock_sub_mp_1 = MagicMock(spec=MeshInterface)
+            mesh_interface_mock_sub_mp_1.configure_mock(**attrs)
+
+            mesh_interface_mock_sub_mp_2 = MagicMock(spec=MeshInterface)
+            mesh_interface_mock_sub_mp_2.configure_mock(**attrs)
+
+            meshes = [
+                geometries_io.Mesh(sub_model_part_1.Name, mesh_interface_mock_sub_mp_1, mesh_description),
+                geometries_io.Mesh(sub_model_part_2.Name, mesh_interface_mock_sub_mp_2, mesh_description_2)
+            ]
+            # this should throw because the entities that are supposed to be added to smp_2 have a different properties-Id, which is not possible!
+
+            with self.assertRaisesRegex(Exception, "Mismatch in properties Ids!\nTrying to use properties with Id 5 with an existing element that has the properties with Id 3"):
+                geometries_io.GeometriesIO.AddMeshes(main_model_part, meshes)
 
 
         ### Auxiliar testing functions ###
@@ -278,9 +485,5 @@ if __name__ == '__main__':
 
 # TODO:
 # - Test for Conditions
-# - Test if things are added to different SubModelParts => elements should be the same
 # - Test with adding elements AND conditions
 # - Test with using Salome instead of the Mock object to make sure the entire Toolchain works
-# - Test maybe for the ALE-case where the geometries are overlapping??? => would require a new salome-testcase... Can be probably done with the existing testcase
-# - Test when the SubModelParts &| Properties are already existing. Also mention in existing tests that the SMPs and the props are being created by the GeometriesIO
-# - Test maybe if same element is being created with different Properties. This check may affect performance and is seldomly used, hence not sure if it should be done...
