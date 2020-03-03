cd5168491b998959f0dd3e03142ede3640c4b8a6
Philipp Bucher <philipp.bucher@tum.de>
2020-03-03 20:14:01 +0100

    adding plugin_logging

---
+++ plugin/plugin_logging.py
@@ -0,0 +1,42 @@
+#  _  __         _          ___       _               ___ _           _
+# | |/ /_ _ __ _| |_ ___ __/ __| __ _| |___ _ __  ___| _ \ |_  _ __ _(_)_ _
+# | ' <| '_/ _` |  _/ _ (_-<__ \/ _` | / _ \ '  \/ -_)  _/ | || / _` | | ' \
+# |_|\_\_| \__,_|\__\___/__/___/\__,_|_\___/_|_|_\___|_| |_|\_,_\__, |_|_||_|
+#                                                               |___/
+# License: BSD License ; see LICENSE
+#
+# Main authors: Philipp Bucher (https://github.com/philbucher)
+#
+
+# python importsimport os
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
+logger = logging.getLogger(__name__)
+logger.debug('loading module')
+
+
+def InitializeLogging(log_file_path, logging_level=logging.DEBUG):
+    # TODO switch the default in the future
+    # logger_level = 2 # default value: 0
+    # logger_levels = { 0 : logging.WARNING,
+    #                 1 : logging.INFO,
+    #                 2 : logging.DEBUG }
+
+    # configuring the root logger, same configuration will be automatically used for other loggers
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging_level)
+    root_logger.handlers.clear() # has to be cleared, otherwise more and more handlers are added if the plugin is reopened
+
+    # logging to console - without timestamp
+    ch = logging.StreamHandler()
+    ch_formatter = logging.Formatter("KSP [%(levelname)8s] %(name)s : %(message)s")
+    ch.setFormatter(ch_formatter)
+    root_logger.addHandler(ch)
+
+    # logging to file - with timestamp
+    fh = RotatingFileHandler(os.path.join(log_file_path, "plugin.log"), maxBytes=5*1024*1024, backupCount=1) # 5 MB
+    fh_formatter = logging.Formatter("[%(asctime)s] [%(levelname)8s] %(name)s : %(message)s", "%Y-%m-%d %H:%M:%S")
+    fh.setFormatter(fh_formatter)
+    root_logger.addHandler(fh)
