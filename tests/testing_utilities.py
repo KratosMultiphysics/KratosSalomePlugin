a1e95152d1da320c92c0543c8351091f22b3c987
Philipp Bucher <philipp.bucher@tum.de>
2020-03-16 10:57:39 +0100

    debug prints

--- tests/testing_utilities.py
+++ tests/testing_utilities.py
@@ -39,9 +39,11 @@ def GetTestsDir():
     return os.path.dirname(os.path.realpath(__file__))
 
 def CheckIfKratosAvailable():
+    print("Checking Kratos availability")
     if "KRATOS_AVAILABLE" in os.environ:
         # this is intended to be used in the CI
         # there "try-except" might lead to an undiscovered failure
+        print('(os.environ["KRATOS_AVAILABLE"] == "1")', (os.environ["KRATOS_AVAILABLE"] == "1"))
         return (os.environ["KRATOS_AVAILABLE"] == "1")
     else:
         try:
