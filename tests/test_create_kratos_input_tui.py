1ae2db43bfb23ab17191ee6a2422da005fd0530f
philbucher <philipp.bucher@tum.de>
2020-04-07 01:43:25 +0200

    first test working

--- tests/test_create_kratos_input_tui.py
+++ tests/test_create_kratos_input_tui.py
@@ -22,18 +22,46 @@ import testing_utilities
 
 if IsExecutedInSalome():
     import create_kratos_input_tui
+    from ks_plugin.utilities import salome_utilities
 
 
 class TestSalomeMesh(testing_utilities.SalomeTestCaseWithBox):
-    def test_xxx(self):
+    def test_mesh_identifier(self):
+        mesh_identifier = salome_utilities.GetSalomeID(self.mesh_hexa.GetMesh())
+
+        sal_mesh = create_kratos_input_tui.SalomeMesh(mesh_identifier, {}, "my_model_part_name")
+
+        self.assertEqual(sal_mesh.mesh_interface.mesh_identifier, mesh_identifier)
+        self.assertEqual(sal_mesh.mesh_description, {})
+        self.assertEqual(sal_mesh.model_part_name, "my_model_part_name")
+
+    def test_mesh_proxy(self):
+        pass
+
+    def test_sub_mesh_proxy(self):
+        pass
+
+    def test_mesh_group(self):
+        pass
+
+    def test_mesh(self):
+        pass
+
+    def test_unpermitted_object(self):
         pass
 
 class TestCreateModelPart(testing_utilities.SalomeTestCaseWithBox):
-    def test_xxx(self):
+    def test_one_mesh(self):
+        pass
+
+    def test_multiple_meshes(self):
         pass
 
 class TestCreateMdpaFile(testing_utilities.SalomeTestCaseWithBox):
-    def test_xxx(self):
+    def test_one_mesh(self):
+        pass
+
+    def test_multiple_meshes(self):
         pass
 
 
