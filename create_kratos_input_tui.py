7cd3b24b650c540763bc8b218da90d5503b97b46
Philipp Bucher <philipp.bucher@tum.de>
2020-05-08 13:23:41 +0200

    minor revert

--- create_kratos_input_tui.py
+++ create_kratos_input_tui.py
@@ -10,6 +10,7 @@
 
 # python imports
 import os
+import logging
 
 # specify logging path (needs to be done before importing the plugin)
 os.environ["KRATOS_SALOME_PLUGIN_LOG_FILE_PATH"] = os.getcwd()
@@ -20,6 +21,8 @@ from ks_plugin import geometries_io
 from ks_plugin.utilities import salome_utilities
 from ks_plugin.mesh_interface import MeshInterface
 from ks_plugin.write_mdpa import WriteMdpa
+
+logger = logging.getLogger(__name__) # done after loading the plugin, which initialized the logging
 
 
 class SalomeMesh(geometries_io.Mesh):
