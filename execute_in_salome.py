4a0b84695df12ad9c7a1f3fdfb0b9912e5d85eea
Philipp <philipp.bucher@tum.de>
2020-05-08 11:10:49 +0200

    WIP first draft refactoring

--- execute_in_salome.py
+++ execute_in_salome.py
@@ -13,6 +13,31 @@ import sys
 import subprocess
 import time, datetime
 
+def Execute(salome_cmd, script_name, *args):
+    info_msg  = 'Executing salome with the following configuration:\n'
+    info_msg += '    \x1b[1;1mSalome-command:\x1b[0m {}\n'.format(salome_cmd)
+    info_msg += '    \x1b[1;1mPython-script:\x1b[0m  {}\n'.format(script_name)
+    info_msg += '    \x1b[1;1mArguments:\x1b[0m      {}\n'.format(args)
+    print(info_msg)
+
+    start_time = time.time()
+
+    sp = subprocess.Popen("{} --shutdown-servers=1 -t {} args:{}".format(salome_cmd, script_name, ", ".join([str(arg) for arg in args])), shell=True, stderr=subprocess.PIPE)
+    _, process_stderr = sp.communicate()
+
+    if process_stderr:
+        print(process_stderr.decode('ascii'))
+
+    # salome < 8.5 does not return the correct return code, hence also check for error message
+    ret_code = sp.returncode != 0 or "ERROR:salomeContext:SystemExit 1 in method _runAppli" in process_stderr.decode('ascii')
+
+    info_msg  = '\x1b[1;1mExecution took: {}'.format((str(datetime.timedelta(seconds=time.time()-start_time))).split(".")[0])
+    info_msg += ' and finished ' + ('\x1b[1;41mnot successful\x1b[0m' if ret_code else '\x1b[1;42msuccessful\x1b[0m')
+    print(info_msg)
+
+    return ret_code == 0 # 0 means successful execution
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         raise Exception("Too few arguments, at least the salome-command and the py-script to be executed have to be given!")
