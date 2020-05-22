1d549d8b02d55e8f46b7c5e3dfb97aad1e159d24
philbucher <philipp.bucher@tum.de>
2020-05-22 10:03:45 +0200

    add error message

--- kratos_salome_plugin/salome_dependent/__init__.py
+++ kratos_salome_plugin/salome_dependent/__init__.py
@@ -1,1 +1,19 @@
-from ..utilities import IsExecutedInSalome
+#  _  __         _          ___       _               ___ _           _
+# | |/ /_ _ __ _| |_ ___ __/ __| __ _| |___ _ __  ___| _ \ |_  _ __ _(_)_ _
+# | ' <| '_/ _` |  _/ _ (_-<__ \/ _` | / _ \ '  \/ -_)  _/ | || / _` | | ' \
+# |_|\_\_| \__,_|\__\___/__/___/\__,_|_\___/_|_|_\___|_| |_|\_,_\__, |_|_||_|
+#                                                               |___/
+# License: BSD License ; see LICENSE
+#
+# Main authors: Philipp Bucher (https://github.com/philbucher)
+#
+
+def __CheckIfIsExecutedInSalome():
+    """This module can only be imported when running inside of Salome
+    It is a "private" function to not pollute the global namespace
+    """
+    from ..utilities import IsExecutedInSalome
+    if not IsExecutedInSalome():
+        raise ImportError("This module can only be imported when running inside of Salome!")
+
+__CheckIfIsExecutedInSalome()
