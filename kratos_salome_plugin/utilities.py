085cd07f611f8ac64b37b820482c59c0eea92fe8
philbucher <philipp.bucher@tum.de>
2020-05-22 12:27:52 +0200

    update doc

--- kratos_salome_plugin/utilities.py
+++ kratos_salome_plugin/utilities.py
@@ -8,34 +8,32 @@
 # Main authors: Philipp Bucher (https://github.com/philbucher)
 #
 
-# This file must NOT have dependencies on other files in the plugin!
+"""
+This file contains utility functions
+NOTE: This file must NOT have dependencies on other files in the plugin!
+"""
 
 # python imports
 import os
 
 def IsExecutedInSalome():
-    """Function to check if the script is being executed inside Salome
-    """
+    """Function to check if the script is being executed inside Salome"""
     return "SALOMEPATH" in os.environ
 
 def GetPluginPath():
-    """This function returns the absolute path to the plugin
-    """
+    """This function returns the absolute path to the plugin"""
     return os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
 
 def GetAbsPathInPlugin(*paths):
-    """This function prepends the path to the plugin to a path given in the input
-    """
+    """This function prepends the path to the plugin to a path given in the input"""
     return os.path.join(GetPluginPath(), *paths)
 
 def GetPythonFilesInDirectory(dir_name):
-    """This function returns a list of all python files in a directory
-    """
+    """This function returns a list of all python files in a directory"""
     return [os.path.relpath(os.path.join(os.path.relpath(dp, dir_name), f)) for dp, _, filenames in os.walk(dir_name) for f in filenames if (f.endswith(".py") and f != "__init__.py")]
 
 def GetPythonModulesInDirectory(dir_name):
-    """This function returns a list of all python modules in a directory
-    """
+    """This function returns a list of all python modules in a directory"""
     # replacing "/" or "\" with "." and removing ".py" extension, e.g.:
     # folder/py_file.py => folder.py_file
     return [f[:-3].replace(os.sep, ".") for f in GetPythonFilesInDirectory(dir_name)]
