a301b52bb1694e41d8a595c832573845ae737e96
Philipp Bucher <philipp.bucher@tum.de>
2020-05-22 12:22:12 +0200

    Prints in geom io (#63)
    * adding prints
    
    * improved prints

--- kratos_salome_plugin/geometries_io.py
+++ kratos_salome_plugin/geometries_io.py
@@ -154,30 +154,40 @@ class GeometriesIO(object):
                     props = model_part_to_add_to.CreateNewProperties(props_id)
                     logger.debug('Creating new Properties with Id {} for "{}"'.format(props_id, entity_name))
 
+                already_existing_entities = 0
+                newly_created_entities = 0
+
                 if entity_name in all_entities: # entities of this type already exist
+                    logger.debug('Entities with name "{}" exist already'.format(entity_name))
+
                     for geometry_id, connectivities in geometries[geometry_type].items():
                         existing_entity = all_entities[entity_name].get(geometry_id)
                         if existing_entity:
                             # an entity was already created from this geometry
                             # therefore NOT creating a new one but adding the existing one
-                            # Note: this does not check the Properties (maybe should, but would probably affect performance)
                             if props_id != existing_entity.Properties.Id:
                                 err_msg  = 'Mismatch in properties Ids!\n'
                                 err_msg += 'Trying to use properties with Id {} '.format(props_id)
                                 err_msg += 'with an existing entity that has the properties with Id {}'.format(existing_entity.Properties.Id)
                                 raise Exception(err_msg)
                             fct_ptr_add_existing_entity(model_part_to_add_to, existing_entity)
+                            already_existing_entities+=1
                         else:
                             # no entity has yet been created from this geometry
                             # hence creating a new one
                             reordered_conn = reorder_conn_fct_ptr(connectivities)
                             id_counter = fct_ptr_create_save_new_entity(entity_name, geometry_id, reordered_conn, props, id_counter)
+                            newly_created_entities+=1
 
                 else: # no entities of this type exist yet, new entities can be added without checking
+                    logger.debug('No entities with name "{}" exist already'.format(entity_name))
                     all_entities[entity_name] = {}
                     for geometry_id, connectivities in geometries[geometry_type].items():
                         reordered_conn = reorder_conn_fct_ptr(connectivities)
                         id_counter = fct_ptr_create_save_new_entity(entity_name, geometry_id, reordered_conn, props, id_counter)
+                        newly_created_entities+=1
+
+                logger.debug('{} new entities were created and {} existed already'.format(newly_created_entities, already_existing_entities))
 
 
 def GetReorderFunction(salome_entity_type):
