b3c8e75b378b9b28013261fa4469891b68eb4ea3
Philipp Bucher <philipp.bucher@tum.de>
2020-05-26 09:13:53 +0200

    missing rename (#77)

--- .github/assert_run_in_salome.py
+++ .github/assert_run_in_salome.py
@@ -13,7 +13,7 @@ import os, sys
 
 # plugin imports
 sys.path.append(os.pardir) # required to be able to do "from plugin import xxx"
-from kratos_salome_plugin.utilities.utils import IsExecutedInSalome
+from kratos_salome_plugin.utilities import IsExecutedInSalome
 
 if len(sys.argv) == 2:
     salome_execution = bool(int(sys.argv[1]))
