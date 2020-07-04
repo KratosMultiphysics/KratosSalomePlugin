1d8628fda56c0b55c66d7c086fccf2f1855fee05
philbucher <philipp.bucher@tum.de>
2020-07-04 22:35:06 +0200

    finished TestPluginMainWindowShortcuts

--- tests/test_plugin_main_window.py
+++ tests/test_plugin_main_window.py
@@ -25,37 +25,80 @@ from testing_utilities import QtTestCase
 from PyQt5.QtCore import Qt
 from PyQt5.QtTest import QTest
 
-class TestPluginMainWindow(QtTestCase):
+class TestPluginMainWindowShortcuts(QtTestCase):
+    """This test checks if the shortcuts are working correctly
+    Useful reference: https://pytest-qt.readthedocs.io/en/1.3.0/
+    """
 
-    def test_Shortcuts(self):
-        main_window = PluginMainWindow()
-        mock_menubar_file_new = MagicMock(side_effect=lambda x:print("   >>> New"))
-        mock_menubar_file_open = MagicMock()
-        mock_menubar_file_save = MagicMock()
-        mock_menubar_file_save_as = MagicMock()
-        mock_menubar_file_close = MagicMock(side_effect=lambda x:print("   >>> HELLO"))
-        mock_menubar_kratos_groups = MagicMock()
+    @classmethod
+    def setUpClass(cls):
+        cls.main_window = PluginMainWindow()
+        cls.mocks = {
+            "file_new"      : MagicMock(),
+            "file_open"     : MagicMock(),
+            "file_save"     : MagicMock(),
+            "file_save_as"  : MagicMock(),
+            "file_close"    : MagicMock(),
+            "kratos_groups" : MagicMock()
+        }
 
-        main_window.actionNew.triggered.connect(mock_menubar_file_new)
-        main_window.actionOpen.triggered.connect(mock_menubar_file_open)
-        main_window.actionSave.triggered.connect(mock_menubar_file_save)
-        main_window.actionSave_As.triggered.connect(mock_menubar_file_save_as)
-        main_window.actionClose.triggered.connect(mock_menubar_file_close)
-        main_window.actionGroups.triggered.connect(mock_menubar_kratos_groups)
+        cls.main_window.actionNew.triggered.connect(cls.mocks["file_new"])
+        cls.main_window.actionOpen.triggered.connect(cls.mocks["file_open"])
+        cls.main_window.actionSave.triggered.connect(cls.mocks["file_save"])
+        cls.main_window.actionSave_As.triggered.connect(cls.mocks["file_save_as"])
+        cls.main_window.actionClose.triggered.connect(cls.mocks["file_close"])
+        cls.main_window.actionGroups.triggered.connect(cls.mocks["kratos_groups"])
 
-        main_window.show()
-        QTest.qWaitForWindowExposed(main_window)
-
-        QTest.keyClick(main_window, Qt.Key_Escape)
-        QTest.keyClicks(main_window, "q", Qt.ControlModifier)
-
-        self.assertTrue(mock_menubar_file_close.called)
-        self.assertEqual(mock_menubar_file_close.call_count, 2)
+        cls.main_window.show()
+        QTest.qWaitForWindowExposed(cls.main_window)
 
 
-        # QTest.keyClicks(main_window, "Ctrl+N")
-        QTest.keyClicks(main_window, "n", Qt.ControlModifier)
-        self.assertEqual(mock_menubar_file_new.call_count, 1)
+    def setUp(self):
+        for mock in self.mocks.values():
+            mock.reset_mock()
+
+    def test_file_new(self):
+        QTest.keyClicks(self.main_window, "n", Qt.ControlModifier)
+        called_mock = "file_new"
+        self.__CheckMockCalls(called_mock)
+
+    def test_file_open(self):
+        QTest.keyClicks(self.main_window, "o", Qt.ControlModifier)
+        called_mock = "file_open"
+        self.__CheckMockCalls(called_mock)
+
+    def test_file_save(self):
+        QTest.keyClicks(self.main_window, "s", Qt.ControlModifier)
+        called_mock = "file_save"
+        self.__CheckMockCalls(called_mock)
+
+    def test_file_save_as(self):
+        QTest.keyClicks(self.main_window, "s", Qt.ControlModifier|Qt.ShiftModifier)
+        called_mock = "file_save_as"
+        self.__CheckMockCalls(called_mock)
+
+    def test_file_close(self):
+        QTest.keyClicks(self.main_window, "q", Qt.ControlModifier)
+        called_mock = "file_close"
+        self.__CheckMockCalls(called_mock)
+
+        QTest.keyClick(self.main_window, Qt.Key_Escape)
+        called_mock = "file_close"
+        self.__CheckMockCalls(called_mock,2)
+
+    def test_kratos_groups(self):
+        QTest.keyClicks(self.main_window, "g", Qt.ControlModifier)
+        called_mock = "kratos_groups"
+        self.__CheckMockCalls(called_mock)
+
+
+    def __CheckMockCalls(self, called_mock, exp_call_count=1):
+        self.assertEqual(self.mocks[called_mock].call_count, exp_call_count, msg='Mock "{}" was not called!'.format(called_mock))
+
+        for mock_name in self.mocks:
+            if mock_name == called_mock:
+                continue
+            self.assertFalse(self.mocks[mock_name].called, msg='Unexpected call for mock "{}": "{}"'.format(called_mock, mock_name))
 
 
 if __name__ == '__main__':
