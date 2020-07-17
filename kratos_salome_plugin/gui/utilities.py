0dcf5d465e169d2c7709f19379398af427c51575
philbucher <philipp.bucher@tum.de>
2020-07-17 18:40:40 +0200

    improvements

--- kratos_salome_plugin/gui/utilities.py
+++ kratos_salome_plugin/gui/utilities.py
@@ -1,8 +1,20 @@
+#  _  __         _          ___       _               ___ _           _
+# | |/ /_ _ __ _| |_ ___ __/ __| __ _| |___ _ __  ___| _ \ |_  _ __ _(_)_ _
+# | ' <| '_/ _` |  _/ _ (_-<__ \/ _` | / _ \ '  \/ -_)  _/ | || / _` | | ' \
+# |_|\_\_| \__,_|\__\___/__/___/\__,_|_\___/_|_|_\___|_| |_|\_,_\__, |_|_||_|
+#                                                               |___/
+# License: BSD License ; see LICENSE
+#
+# Main authors: Philipp Bucher (https://github.com/philbucher)
+#
 
+# qt imports
+from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtCore import Qt
+from PyQt5.QtTest import QTest
 
-
-def ShowInformativeMessageBox(
+def CreateInformativeMessageBox(
     text,
     icon,
     informative_text="",
@@ -26,4 +38,15 @@ def ShowInformativeMessageBox(
     if detailed_text:
         mbx.setDetailedText(detailed_text)
 
-    mbx.exec()
+    return mbx
+
+# for testing / debugging
+if __name__ == '__main__':
+    import sys
+    from PyQt5.QtWidgets import QApplication
+    app = QApplication(sys.argv)
+
+    CreateInformativeMessageBox("some_text", "Information", "sss", "ddddd").exec()
+    print("after")
+
+    sys.exit(app.exec_())
