615f099edd1ab77e26c3239dd3e98ba4a5651c7e
Philipp Bucher <philipp.bucher@tum.de>
2020-07-20 02:30:22 +0200

    Show events in messageboxes (#118)
    * improved PrintObjectInfo to also print types
    
    * showing exceptions working
    
    * adding gui.utilities
    
    * added tests
    
    * improvements
    
    * minor
    
    * cleanup
    
    * adding comment
    
    * test for logging
    
    * adding aux file
    
    * refactor exception logging
    
    * adding gui.utilities to reload_modules
    
    * fix conversion
    
    * fix disabled logger
    
    * add to path
    
    * tmp comment test
    
    * fix path
    
    * print path
    
    * tmp disabled check
    
    * fix for Win
    
    * fix for python3.5
    
    * fix logging test
    
    * debug print
    
    * another debug check
    
    * fix debug
    
    * now should work
    
    * commenting debug
    
    * pring back check
    
    * cleaning and fixing test
    
    * remove superfluous test
    
    * improve check
    
    * try sth
    
    * more tests
    
    * finalize MessageBoxHnadler
    
    * add test
    
    * more testing
    
    * update after master merge
    
    * improve environment handling
    
    * finish testing of logging
    
    * missing skiptest
    
    * minor
    
    * maybe fix later
    
    * fix

--- kratos_salome_plugin/plugin_logging.py
+++ kratos_salome_plugin/plugin_logging.py
@@ -9,13 +9,26 @@
 #
 
 # python imports
+import os
+import sys
+import traceback
 import logging
-import os, sys
 from logging.handlers import RotatingFileHandler
+logger = logging.getLogger("KRATOS SALOME PLUGIN")
+
+# qt imports
+try:
+    from PyQt5 import QtWidgets
+    qt_available = True
+except:
+    qt_available = False
 
 # plugin imports
 from . import IsExecutedInSalome
 from .utilities import GetAbsPathInPlugin
+if qt_available:
+    from kratos_salome_plugin.gui.utilities import CreateInformativeMessageBox
+
 
 class _AnsiColorStreamHandler(logging.StreamHandler):
     # adapted from https://gist.github.com/mooware/a1ed40987b6cc9ab9c65
@@ -51,6 +64,52 @@ class _AnsiColorStreamHandler(logging.StreamHandler):
         return text.replace(record.levelname, self.__ColorLevel(record))
 
 
+class _MessageBoxLogHandler(logging.Handler):
+    """This handler shows critical problems in a messagebox
+    It is supposed to be used when running in salome to make the user aware
+    """
+    def __init__(self):
+        """Only logs with level CRITICAL are emitted"""
+        super().__init__(logging.CRITICAL)
+
+    def emit(self, record):
+        """Open a messagebox showing the critical message"""
+        informative_text = 'Please report this problem under "https://github.com/philbucher/KratosSalomePlugin"'
+        CreateInformativeMessageBox(
+            "Critical event occurred!",
+            'Critical',
+            informative_text,
+            record.getMessage()).exec()
+
+
+def _HandleUnhandledException(exc_type, exc_value, exc_traceback):
+    """Handler for unhandled exceptions that will write to the logs
+    taken from: https://www.scrygroup.com/tutorial/2018-02-06/python-excepthook-logging/
+    TODO: check if this also works properly in GUI
+    """
+    if issubclass(exc_type, KeyboardInterrupt):
+        # call the default excepthook saved at __excepthook__
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    if qt_available:
+        if QtWidgets.QApplication.instance() is not None: # check if a GUI exists
+            text = 'An unhandled excepition occured!'
+            informative_text = 'Please report this problem under "https://github.com/philbucher/KratosSalomePlugin"'
+
+            detailed_text  = 'Details of the error:\n'
+            detailed_text += 'Type: {}\n\n'.format(exc_type.__name__)
+            detailed_text += 'Message: {}\n\n'.format(exc_value)
+            detailed_text += 'Traceback:\n'
+            for line in traceback.format_tb(exc_traceback):
+                detailed_text += '  ' + line
+            detailed_text = detailed_text.rstrip("\n")
+
+            CreateInformativeMessageBox(text, 'Critical', informative_text, detailed_text).exec()
+
+    logger.exception("Unhandled exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+
 def InitializeLogging(logging_level=logging.DEBUG):
     # TODO switch the default in the future
     # TODO this should come from the config file
@@ -62,7 +121,7 @@ def InitializeLogging(logging_level=logging.DEBUG):
     # configuring the root logger, same configuration will be automatically used for other loggers
     root_logger = logging.getLogger()
 
-    disable_logging = os.getenv("KRATOS_SALOME_PLUGIN_DISABLE_LOGGING", False)
+    disable_logging = bool(int(os.getenv("KRATOS_SALOME_PLUGIN_DISABLE_LOGGING", False)))
 
     if disable_logging:
         # this is intended for disabling the logger during testing, because some tests would generate output
@@ -99,19 +158,7 @@ def InitializeLogging(logging_level=logging.DEBUG):
         fh.setFormatter(fh_formatter)
         root_logger.addHandler(fh)
 
-        def handle_unhandled_exception(exc_type, exc_value, exc_traceback):
-            """Handler for unhandled exceptions that will write to the logs
-            taken from: https://www.scrygroup.com/tutorial/2018-02-06/python-excepthook-logging/
-            TODO:
-                - check if this also works properly in GUI
-                - might need some modifications for multiprocessing/threading (see link)
-            """
-            if issubclass(exc_type, KeyboardInterrupt):
-                # call the default excepthook saved at __excepthook__
-                sys.__excepthook__(exc_type, exc_value, exc_traceback)
-                return
+        if IsExecutedInSalome():
+            root_logger.addHandler(_MessageBoxLogHandler())
 
-            root_logger.error("Unhandled exception", exc_info=(exc_type, exc_value, exc_traceback))
-            sys.__excepthook__(exc_type, exc_value, exc_traceback) # re-raise exception after looging
-
-        sys.excepthook = handle_unhandled_exception
+        sys.excepthook = _HandleUnhandledException
