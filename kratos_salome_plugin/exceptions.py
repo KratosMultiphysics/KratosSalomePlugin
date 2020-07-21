b63fe70df2259a9b2895453463ded6e97274286c
philbucher <philipp.bucher@tum.de>
2020-07-21 16:02:03 +0200

    adding user-error

---
+++ kratos_salome_plugin/exceptions.py
@@ -0,0 +1,15 @@
+#  _  __         _          ___       _               ___ _           _
+# | |/ /_ _ __ _| |_ ___ __/ __| __ _| |___ _ __  ___| _ \ |_  _ __ _(_)_ _
+# | ' <| '_/ _` |  _/ _ (_-<__ \/ _` | / _ \ '  \/ -_)  _/ | || / _` | | ' \
+# |_|\_\_| \__,_|\__\___/__/___/\__,_|_\___/_|_|_\___|_| |_|\_,_\__, |_|_||_|
+#                                                               |___/
+# License: BSD License ; see LICENSE
+#
+# Main authors: Philipp Bucher (https://github.com/philbucher)
+#
+
+"""
+This file defines the custom exceptions used in the plugin
+"""
+
+class UserInputError(Exception): pass
