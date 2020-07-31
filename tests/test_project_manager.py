589b729366313d57b38bb5dbdd88ed45cf5c90bb
philbucher <philipp.bucher@tum.de>
2020-07-31 09:57:26 +0200

    refactor test case

--- tests/test_project_manager.py
+++ tests/test_project_manager.py
@@ -37,7 +37,7 @@ class TestProjectManager(QtTestCase):
     @patch('salome_version.getVersions', return_value=[1,2,3])
     @patch('salome.myStudy.SaveAs', side_effect=CreateHDFStudyFile)
     def test_SaveProject_mocked_salome(self, mock_save_study, mock_version):
-        self.__execute_test_SaveProject()
+        _ExecuteTestSaveProject(self)
 
         self.assertTrue(mock_version.called)
         self.assertEqual(mock_version.call_count, 1)
@@ -47,7 +47,7 @@ class TestProjectManager(QtTestCase):
 
     @unittest.skipUnless(IsExecutedInSalome(), "Test requires Salome!")
     def test_SaveProject_real_salome(self):
-        self.__execute_test_SaveProject()
+        _ExecuteTestSaveProject(self)
 
     @patch('salome_version.getVersions', return_value=[1,2,3])
     @patch('salome.myStudy.SaveAs', side_effect=CreateHDFStudyFile)
@@ -62,10 +62,13 @@ class TestProjectManager(QtTestCase):
         main_kratos_py_path = project_dir / "MainKratos.py"
         proj_params_json_path = project_dir / "ProjectParameters.json"
 
+        plugin_data_path = project_dir / "plugin_data.json"
+        salome_study_path = project_dir / "salome_study.hdf"
+
         main_kratos_py_path.touch()
         proj_params_json_path.touch()
 
-        self.__execute_test_SaveProject(project_name)
+        _ExecuteTestSaveProject(self, project_name)
 
         self.assertTrue(mock_version.called)
         self.assertEqual(mock_version.call_count, 1)
@@ -76,54 +79,62 @@ class TestProjectManager(QtTestCase):
         # make sure the previously existing file wasn't deleted aka the folder wasn't removed and recreated
         self.assertTrue(main_kratos_py_path.is_file())
         self.assertTrue(proj_params_json_path.is_file())
+        self.assertTrue(plugin_data_path.is_file())
+        self.assertTrue(salome_study_path.is_file())
 
     @patch('salome_version.getVersions', return_value=[1,2,3])
     @patch('salome.myStudy.SaveAs', side_effect=CreateHDFStudyFile)
     @patch('salome.myStudy.Open', return_value=True)
     @patch('kratos_salome_plugin.salome_study_utilities.GetNumberOfObjectsInStudy', return_value=0)
-    def test_OpenProject(self, mock_num_objs_study, mock_open_study, mock_save_study, mock_version):
-        # TODO add a test with real salome, i.e. where it is not patched!
+    def test_OpenProject_mocked_salome(self, mock_num_objs_study, mock_open_study, mock_save_study, mock_version):
         manager = ProjectManager()
 
         # first save the project
-        project_name = Path("project_for_opening")
-        self.__execute_test_SaveProject(project_name)
+        project_name = _ExecuteTestSaveProject(self)
         project_dir = project_name.with_suffix(".ksp")
 
         # then open it again and check if is is the same
         self.assertTrue(manager.OpenProject(project_dir))
         # TODO implement checks (GroupsManager and App should be checked)
 
+    @unittest.skipUnless(IsExecutedInSalome(), "Test requires Salome!")
+    def test_OpenProject_real_salome(self):
+        pass
 
-    def __execute_test_SaveProject(self, project_name=Path("my_own_project")):
-        project_dir = project_name.with_suffix(".ksp")
+def _ExecuteTestSaveProject(test_case, project_name=None):
+    if not project_name:
+        project_name = Path(test_case._testMethodName)
 
-        self.addCleanup(lambda: DeleteDirectoryIfExisting(project_dir))
+    project_dir = project_name.with_suffix(".ksp")
 
-        manager = ProjectManager()
+    test_case.addCleanup(lambda: DeleteDirectoryIfExisting(project_dir))
 
-        self.assertTrue(manager.SaveProject(project_name))
+    manager = ProjectManager()
 
-        plugin_data_path = project_dir / "plugin_data.json"
+    test_case.assertTrue(manager.SaveProject(project_name))
 
-        # check the files were created
-        self.assertTrue(project_dir.is_dir())
-        self.assertTrue(plugin_data_path.is_file())
-        self.assertTrue(project_dir.joinpath("salome_study.hdf").is_file())
+    plugin_data_path = project_dir / "plugin_data.json"
 
-        # check content of "plugin_data.json"
-        with open(plugin_data_path, 'r') as plugin_data_file:
-            plugin_data = json.load(plugin_data_file)
+    # check the files were created
+    test_case.assertTrue(project_dir.is_dir())
+    test_case.assertTrue(plugin_data_path.is_file())
+    test_case.assertTrue(project_dir.joinpath("salome_study.hdf").is_file())
 
-        self.assertIn("general", plugin_data)
-        self.assertIn("groups", plugin_data)
-        self.assertNotIn("application", plugin_data) # no app was loaded hence this shouldn't exist
+    # check content of "plugin_data.json"
+    with open(plugin_data_path, 'r') as plugin_data_file:
+        plugin_data = json.load(plugin_data_file)
 
-        general = plugin_data["general"]
-        self.assertIn("version_plugin", general)
-        self.assertIn("version_salome", general)
-        self.assertIn("creation_time", general)
-        self.assertIn("operating_system", general)
+    test_case.assertIn("general", plugin_data)
+    test_case.assertIn("groups", plugin_data)
+    test_case.assertNotIn("application", plugin_data) # no app was loaded hence this shouldn't exist
+
+    general = plugin_data["general"]
+    test_case.assertIn("version_plugin", general)
+    test_case.assertIn("version_salome", general)
+    test_case.assertIn("creation_time", general)
+    test_case.assertIn("operating_system", general)
+
+    return project_name
 
 
 if __name__ == '__main__':
