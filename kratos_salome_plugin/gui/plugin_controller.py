2c67fd253956389ae0b8f720de0217e059be7101
Philipp Bucher <philipp.bucher@tum.de>
2020-08-01 23:38:25 +0200

    [GUI] Adding save and open (#132)
    * making initial connection
    
    * test for SaveAs
    
    * advances in PluginController
    
    * more test
    
    * refactor CreateHDFStudyFile
    
    * test save-as aborted
    
    * improve log levels
    
    * testing logs
    
    * test save_as failed
    
    * test logs for abort save
    
    * minor
    
    * hotfix
    
    * examples for logging
    
    * first log exception then show in msgbox
    
    * minor improvements
    
    * initial add custom exceptions
    
    * using UserInputError
    
    * updated test
    
    * mrege error
    
    * finished _Save
    
    * tests for _Save
    
    * adding test for _New
    
    * advances
    
    * improve test
    
    * using statusbar
    
    * refactor
    
    * improvements
    
    * rename member
    
    * updated tests
    
    * adding documentation
    
    * added test for Close
    
    * adding check in test
    
    * empty tests for Open
    
    * added tests for open
    
    * started salome tests
    
    * finished tests
    
    * using QtTestCase
    
    * Revert "improve test"
    
    This reverts commit 46724238f864f67f2c381dd6dbde0f407b4b1fc4.
    
    * skip some tests in py 3.5

--- kratos_salome_plugin/gui/plugin_controller.py
+++ kratos_salome_plugin/gui/plugin_controller.py
@@ -14,82 +14,164 @@ https://www.tutorialspoint.com/software_architecture_design/interaction_oriented
 """
 
 # python imports
-import os
+from pathlib import Path
 import webbrowser
 import logging
 logger = logging.getLogger(__name__)
 
 # plugin imports
+from kratos_salome_plugin.exceptions import UserInputError
 from kratos_salome_plugin.gui.plugin_main_window import PluginMainWindow
 from kratos_salome_plugin.gui.about import ShowAbout
+from kratos_salome_plugin.gui.project_manager import ProjectManager
+from kratos_salome_plugin.gui.project_path_handler import ProjectPathHandler
 
 def ShowNotImplementedMessage():
     from PyQt5.QtWidgets import QMessageBox
     QMessageBox.information(None, "Not implemented", "This is not yet implemented")
 
+
 class PluginController(object):
     def __init__(self):
         logger.debug('Creating PluginController')
-        self.main_window = PluginMainWindow()
+        self._main_window = PluginMainWindow()
+        self.__InitializeMembers()
 
         self.__ConnectMainWindow()
 
     def ShowMainWindow(self) -> None:
         """show main window"""
-        self.main_window.ShowOnTop()
+        self._main_window.ShowOnTop()
 
 
-    def __ConnectMainWindow(self):
+    def __InitializeMembers(self) -> None:
+        """completely reinitialize members to clean them"""
+        self._project_manager = ProjectManager()
+        self._project_path_handler = ProjectPathHandler()
+        self._previous_save_path = None
+
+
+    def __ConnectMainWindow(self) -> None:
         ### File menu
-        self.main_window.actionNew.triggered.connect(self._New)
-        self.main_window.actionOpen.triggered.connect(self._Open)
-        self.main_window.actionSave.triggered.connect(self._Save)
-        self.main_window.actionSave_As.triggered.connect(self._SaveAs)
-        self.main_window.actionSettings.triggered.connect(self._Settings)
-        self.main_window.actionClose.triggered.connect(self._Close)
+        self._main_window.actionNew.triggered.connect(self._New)
+        self._main_window.actionOpen.triggered.connect(self._Open)
+        self._main_window.actionSave.triggered.connect(self._Save)
+        self._main_window.actionSave_As.triggered.connect(self._SaveAs)
+        self._main_window.actionSettings.triggered.connect(self._Settings)
+        self._main_window.actionClose.triggered.connect(self._Close)
 
         ### Kratos menu
-        self.main_window.actionGroups.triggered.connect(self._Groups)
-        self.main_window.actionLoad_Application.triggered.connect(self._LoadApplication)
-        self.main_window.actionImport_MDPA.triggered.connect(self._ImportMdpa)
+        self._main_window.actionGroups.triggered.connect(self._Groups)
+        self._main_window.actionLoad_Application.triggered.connect(self._LoadApplication)
+        self._main_window.actionImport_MDPA.triggered.connect(self._ImportMdpa)
 
         ### Help menu
-        self.main_window.actionAbout.triggered.connect(lambda: ShowAbout(self.main_window))
-        self.main_window.actionWebsite.triggered.connect(lambda: webbrowser.open("https://github.com/philbucher/KratosSalomePlugin"))
+        self._main_window.actionAbout.triggered.connect(lambda: ShowAbout(self._main_window))
+        self._main_window.actionWebsite.triggered.connect(lambda: webbrowser.open("https://github.com/philbucher/KratosSalomePlugin"))
 
         ### Startup buttons
-        self.main_window.pushButton_Open.clicked.connect(self._Open)
-        self.main_window.pushButton_Load_Application.clicked.connect(self._LoadApplication)
+        self._main_window.pushButton_Open.clicked.connect(self._Open)
+        self._main_window.pushButton_Load_Application.clicked.connect(self._LoadApplication)
 
 
     ### File menu
-    def _New(self):
+    def _New(self) -> None:
+        """Create new project. Salome study is untouched
+        # TODO check for unsaved changes
+        """
+        self.__InitializeMembers()
+
+    def _Open(self) -> None:
+        """Open a project. User is asked for the path to the project folder
+        # TODO check for unsaved changes
+        """
+        try:
+            path = self._project_path_handler.GetOpenPath(self._main_window)
+        except UserInputError as e:
+            msg = "User input error while opening project: {}".format(e)
+            logger.warning(msg)
+            self._main_window.StatusBarWarning(msg)
+            return
+
+        if path == Path("."): # this means the dialog was aborted, do nothing in this case
+            msg = "Opening was aborted"
+            logger.info(msg)
+            self._main_window.StatusBarWarning(msg)
+            return
+
+        open_successful = self._project_manager.OpenProject(path)
+
+        if open_successful:
+            msg = 'Successfully opened project from "{}"'.format(path)
+            logger.info(msg)
+            self._main_window.StatusBarInfo(msg)
+        else:
+            logger.critical('Failed to open project from "%s"!', path)
+
+    def _Save(self) -> None:
+        """Save the project. If it was saved before the same path is reused.
+        Otherwise SaveAs is used and the user is asked to provide a path for saving
+        """
+        if self._previous_save_path:
+            logger.info("Saving project with previous save path ...")
+            self.__SaveProject(self._previous_save_path)
+        else: # project was not previously saved
+            self._SaveAs()
+
+    def _SaveAs(self) -> None:
+        """Save the project. The user is asked to provide a path for saving.
+        If the dialog for giving the path is aborted then nothing happens
+        """
+        logger.info("Saving project as ...")
+
+        path = self._project_path_handler.GetSavePath(self._main_window)
+
+        if path == Path("."): # this means the dialog was aborted, do nothing in this case
+            msg = "Saving was aborted"
+            logger.info(msg)
+            self._main_window.StatusBarWarning(msg)
+            return
+
+        save_successful = self.__SaveProject(path)
+        if save_successful:
+            self._previous_save_path = path # saving the path such that it can be reused
+
+
+    def _Settings(self) -> None:
+        raise Exception("Example of showing exception in messagebox")
         ShowNotImplementedMessage()
 
-    def _Open(self):
-        ShowNotImplementedMessage()
-
-    def _Save(self):
-        ShowNotImplementedMessage()
-
-    def _SaveAs(self):
-        ShowNotImplementedMessage()
-
-    def _Settings(self):
-        ShowNotImplementedMessage()
-
-    def _Close(self):
-        self.main_window.close()
+    def _Close(self) -> None:
+        """Close the plugin window
+        State is preserved!
+        # TODO check for unsaved changes
+        """
+        self._main_window.close()
 
     ### Kratos menu
-    def _Groups(self):
+    def _Groups(self) -> None:
+        logger.critical("This is a critical messagbox example")
         ShowNotImplementedMessage()
 
-    def _LoadApplication(self):
+    def _LoadApplication(self) -> None:
         ShowNotImplementedMessage()
 
-    def _ImportMdpa(self):
+    def _ImportMdpa(self) -> None:
         ShowNotImplementedMessage()
+
+    def __SaveProject(self, path: Path) -> bool:
+        """internal function for saving the project
+        and issue the appropriate infos for the user
+        returns if saving was successful
+        """
+        save_successful = self._project_manager.SaveProject(path)
+        if save_successful:
+            msg = 'Saved project under "{}"'.format(path)
+            logger.info(msg)
+            self._main_window.StatusBarInfo(msg)
+        else:
+            logger.critical('Failed to save project under "%s"!', path)
+        return save_successful
 
 
 # for testing / debugging
@@ -98,5 +180,5 @@ if __name__ == '__main__':
     from PyQt5.QtWidgets import QApplication
     app = QApplication(sys.argv)
     contr = PluginController()
-    contr.main_window.show()
+    contr._main_window.show()
     sys.exit(app.exec_())
