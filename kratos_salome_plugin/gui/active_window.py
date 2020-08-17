5126881f603658a940d72b231fdbe1174e4596b4
philbucher <philipp.bucher@tum.de>
2020-08-17 22:50:31 +0200

    adding description

--- kratos_salome_plugin/gui/active_window.py
+++ kratos_salome_plugin/gui/active_window.py
@@ -1,1 +1,18 @@
+#  _  __         _          ___       _               ___ _           _
+# | |/ /_ _ __ _| |_ ___ __/ __| __ _| |___ _ __  ___| _ \ |_  _ __ _(_)_ _
+# | ' <| '_/ _` |  _/ _ (_-<__ \/ _` | / _ \ '  \/ -_)  _/ | || / _` | | ' \
+# |_|\_\_| \__,_|\__\___/__/___/\__,_|_\___/_|_|_\___|_| |_|\_,_\__, |_|_||_|
+#                                                               |___/
+# License: BSD License ; see LICENSE
+#
+# Main authors: Philipp Bucher (https://github.com/philbucher)
+#
+
+"""
+The currently active window is saved in a global variable
+This is necessary in case the window gets minimized
+I.e. when the plugin is reopened (with a previously minimized window)
+then the last opened window is shown again instead of creating again the base window
+"""
+
 ACTIVE_WINDOW=None
