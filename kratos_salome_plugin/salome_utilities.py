c8919718fc30c0f502436296655610ba132a2f18
philbucher <philipp.bucher@tum.de>
2020-08-21 13:45:05 +0200

    improve salome utilities

--- kratos_salome_plugin/salome_utilities.py
+++ kratos_salome_plugin/salome_utilities.py
@@ -14,46 +14,57 @@ NOTE: This file must NOT have dependencies on other files in the plugin!
 """
 
 # python imports
+from typing import List
 import logging
 logger = logging.getLogger(__name__)
 
 # salome imports
 import salome
 import salome_version
+import SalomePyQt
+sgPyQt = SalomePyQt.SalomePyQt()
 
-def GetVersionMajor():
+def GetVersionMajor() -> int:
     """returns the major version of Salome as int"""
     return salome_version.getVersions()[0]
 
-def GetVersionMinor():
+def GetVersionMinor() -> int:
     """returns the minor version of Salome as int"""
     return salome_version.getVersions()[1]
 
-def GetVersionPatch():
+def GetVersionPatch() -> int:
     """returns the patch version of Salome as int"""
     return salome_version.getVersions()[2]
 
-def GetVersions():
+def GetVersions() -> List[int]:
     """returns the versions of the plugin as a list of integers
     e.g. [9,4,0]
     """
     return salome_version.getVersions()
 
-def GetVersionString():
+def GetVersionString() -> str:
     """returns the versions of the plugin as a string with versions separated by "."
     e.g. "9.4.0"
     """
     return salome_version.getVersion()
 
-def HasDesktop():
+def HasDesktop() -> bool:
     """if Salome is executed with (aka GUI mode) or without Desktop (aka TUI mode)"""
     return salome.sg.hasDesktop()
 
-def ExecutionMode():
+def ExecutionMode() -> str:
     """mode in which Salome is executed, GUI or TUI"""
     return "GUI" if HasDesktop() else "TUI"
 
-def GetSalomeObjectReference(object_identifier, log_if_not_existing=True):
+def GetActiveComponent() -> str:
+    """returns the name of the currently active component
+    e.g. SMESH or GEOM
+    if no component is activated yet (i.e. right after opening Salome)
+    then an empty string is returned
+    """
+    return sgPyQt.getActiveComponent()
+
+def GetSalomeObjectReference(object_identifier: str, log_if_not_existing: bool=True):
     obj_ref = salome.myStudy.FindObjectID(object_identifier)
 
     if obj_ref is None and log_if_not_existing:
@@ -61,14 +72,14 @@ def GetSalomeObjectReference(object_identifier, log_if_not_existing=True):
 
     return obj_ref
 
-def GetSalomeObject(object_identifier):
+def GetSalomeObject(object_identifier: str):
     return GetSalomeObjectReference(object_identifier).GetObject()
 
-def GetObjectName(object_identifier):
+def GetObjectName(object_identifier: str) -> str:
     return GetSalomeObjectReference(object_identifier).GetName()
 
-def ObjectExists(object_identifier):
+def ObjectExists(object_identifier: str) -> bool:
     return (GetSalomeObjectReference(object_identifier, False) is not None)
 
-def GetSalomeID(salome_object):
+def GetSalomeID(salome_object) -> str:
     return salome.ObjectToID(salome_object)
