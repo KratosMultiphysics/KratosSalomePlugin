2eb4929ebfd1b919ebe5a3e471762b0ce4607083
philbucher <philipp.bucher@tum.de>
2020-08-21 13:42:19 +0200

    clean group

--- kratos_salome_plugin/gui/group.py
+++ kratos_salome_plugin/gui/group.py
@@ -17,11 +17,8 @@ logger = logging.getLogger(__name__)
 
 # for showing group: https://docs.salome-platform.org/latest/gui/GUI/text_user_interface.html
 
-class Group(object):
+class Group:
     def __init__(self, name, mesh_identifier, entity_type):
         self.name = name
         self.mesh_identifier = mesh_identifier
         self.entity_type = entity_type
-
-    def GetStatusColor(self):
-        return "orange"
